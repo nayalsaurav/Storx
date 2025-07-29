545e61bd8c870af4b841a4d355733f03035dbd92
Saurav Nayal <nayalsaurav@gmail.com>
2025-07-29 18:56:26 +0530

    added api routes for files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Provider from "@/components/provider";
+import Provider from "@/app/providers";
 import { Toaster } from "@/components/ui/sonner";
 const geistSans = Geist({
   variable: "--font-geist-sans",
